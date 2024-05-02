ad2e02b6cefb5b5d11ab912ba77227b1d6c96512
Stephen Weatherhead <stephenw42@outlook.com>
2024-05-02 21:57:47 +0100

    Added control flow project

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please enter the number of square metres you require:");
+int squareMetres = int.Parse(Console.ReadLine());
+
+if(!IsEligibleForAutoApply(squareMetres))
+{
+    Console.WriteLine("Sorry, you are not eligible for auto-apply. Please contact a sales representative for a quote");
+    Console.ReadLine();
+    return;
+}
+
+
+bool IsEligibleForAutoApply(int squareMetres)
+{
+    return squareMetres >= 2 && squareMetres <= 4;
+}
