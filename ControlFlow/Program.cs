2a17c3dff80c7f79beb5b4d10ba9837c687f2c20
Stephen Weatherhead <stephenw42@outlook.com>
2024-05-03 21:55:17 +0100

    Added switch example

--- ControlFlow/Program.cs
+++ ControlFlow/Program.cs
@@ -8,6 +8,39 @@ if(!IsEligibleForAutoApply(squareMetres))
     return;
 }
 
+Console.WriteLine();
+Console.WriteLine("Please choose a category for your wares");
+Console.WriteLine("1 - Clothes");
+Console.WriteLine("2 - Electronics");
+Console.WriteLine("3 - Antiques");
+Console.WriteLine("4 - Art");
+Console.WriteLine("5 - Other");
+int category = int.Parse(Console.ReadLine());
+
+// interesting note: switch statement does not work for float and double
+switch(category)
+{
+    case 1:
+        Console.WriteLine("You chose: Clothes");
+        break;
+    case 2:
+        Console.WriteLine("You chose: Electronics");
+        break;
+    case 3:
+        Console.WriteLine("You chose: Antiques");
+        break;
+    case 4:
+        Console.WriteLine("You chose: Art");
+        break;
+    case 5:
+        Console.WriteLine("You chose: Other");
+        break;
+    default:
+        Console.WriteLine("Could not recognise category");
+        Console.ReadLine();
+        return;
+}
+Console.ReadLine();
 
 bool IsEligibleForAutoApply(int squareMetres)
 {
