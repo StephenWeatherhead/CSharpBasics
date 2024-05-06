009dbc86266355ee893518cdf45c30d87baae25c
Stephen Weatherhead <stephenw42@outlook.com>
2024-05-06 21:46:08 +0100

    Added intial third eye program

---
+++ TheThirdEye/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TheThirdEye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The third eye is said to provide perception beyond ordinary sight.");
+            Console.WriteLine("To access inner realms and spaces of higher consciousness.");
+            Console.ReadLine();
+        }
+    }
+}
