3f09881479ac8c4871a9e43e435732ece08b9f25
Stephen Weatherhead <stephenw42@outlook.com>
2024-05-09 22:58:18 +0100

    Added print details method to gamer class

--- MegaExtremeGamerAwesomenessNetwork/Gamer.cs
+++ MegaExtremeGamerAwesomenessNetwork/Gamer.cs
@@ -8,7 +8,7 @@ namespace MegaExtremeGamerAwesomenessNetwork
 {
     internal class Gamer
     {
-        public Guid meganId;
+        public Guid megId;
         public string gamerTag;
 
         public int kills;
@@ -27,11 +27,18 @@ namespace MegaExtremeGamerAwesomenessNetwork
             Console.WriteLine($"{gamerTag} made {numberOfKills} kill(s)!");
         }
 
-        public double CalculateKDRatio()
+        public double CalculateKD()
         {
             double ratio = (double)kills / (double)(deaths + 1);
             Console.WriteLine($"{gamerTag} has a K/D ratio of {ratio}");
             return ratio;
+        }
+
+        public void PrintGamerDeets()
+        {
+            Console.WriteLine($"MegId: {megId} Tag: {gamerTag}");
+            Console.WriteLine($"Kills: {kills} Deaths: {deaths}");
+            Console.WriteLine($"Cake Day: {cakeDay.ToShortDateString()}");
         }
     }
 }
--- MegaExtremeGamerAwesomenessNetwork/Program.cs
+++ MegaExtremeGamerAwesomenessNetwork/Program.cs
@@ -6,6 +6,11 @@
         {
             Console.WriteLine("Hello! and welcome to THE MEGA EXTREME GAMER AWESOMENESS NETWORK!");
             Console.WriteLine("Or MEGAN for short.");
+            Gamer jemma = new Gamer();
+            jemma.Kill();
+            jemma.Kill(12);
+            jemma.PrintGamerDeets();
+            jemma.CalculateKD();
             Console.ReadLine();
         }
     }
