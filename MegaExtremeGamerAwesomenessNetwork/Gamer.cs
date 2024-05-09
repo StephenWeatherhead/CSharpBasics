0fa378353f6ed0ede3641bf1736c53cf92f52249
Stephen Weatherhead <stephenw42@outlook.com>
2024-05-09 22:49:33 +0100

    Added MEGAN project with initial gamer class

---
+++ MegaExtremeGamerAwesomenessNetwork/Gamer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaExtremeGamerAwesomenessNetwork
+{
+    internal class Gamer
+    {
+        public Guid meganId;
+        public string gamerTag;
+
+        public int kills;
+        public int deaths;
+
+        public DateTime cakeDay;
+
+        public void Kill()
+        {
+            Kill(1);
+        }
+
+        public void Kill(int numberOfKills)
+        {
+            kills += numberOfKills;
+            Console.WriteLine($"{gamerTag} made {numberOfKills} kill(s)!");
+        }
+
+        public double CalculateKDRatio()
+        {
+            double ratio = (double)kills / (double)(deaths + 1);
+            Console.WriteLine($"{gamerTag} has a K/D ratio of {ratio}");
+            return ratio;
+        }
+    }
+}
---
+++ MegaExtremeGamerAwesomenessNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MegaExtremeGamerAwesomenessNetwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! and welcome to THE MEGA EXTREME GAMER AWESOMENESS NETWORK!");
+            Console.WriteLine("Or MEGAN for short.");
+            Console.ReadLine();
+        }
+    }
+}
