d14488fc786b0e3a23b1df8f1d8a41342e39b129
Stephen Weatherhead <stephenw42@outlook.com>
2024-06-07 21:59:54 +0100

    Added intial shell game project

---
+++ ShellGame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ShellGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello weary traveller! Care to make a wager? See these cups here?");
+            Console.WriteLine("See this ball? Guess where the ball ends up...");
+            Console.WriteLine();
+            Console.WriteLine("|Z| |Z| |Z|");
+            Console.WriteLine(" O         ");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to shuffle...");
+            Console.ReadLine();
+        }
+    }
+}
