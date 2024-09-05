f3ac2671b0de9af3da24e460474383420fdfa04a
Stephen Weatherhead <stephenw42@outlook.com>
2024-09-05 23:06:37 +0100

    Budget app

---
+++ BudgetApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BudgetApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Budget myBudget = new Budget("Oreo Budget", 100, 
+                DateOnly.FromDateTime(DateTime.Today),
+                DateOnly.FromDateTime(DateTime.Today.AddDays(30)));
+
+            Console.WriteLine($"# {myBudget.Name}");
+            Console.WriteLine($"Total Budget: {myBudget.Amount:C}");
+            Console.WriteLine($"Daily Spend: {myBudget.CalculateDailySpend():C}");
+            Console.WriteLine($"Weekly Spend: {myBudget.CalculateWeeklySpend():C}");
+        }
+    }
+    public class Budget(string name, double amount, DateOnly startDate, DateOnly endDate)
+    {
+        public string Name { get => name; private set { name = value; } }
+        public double Amount { get => amount; private set { amount = value; } }
+        public DateOnly StartDate { get => startDate; set => startDate = value; }
+        public DateOnly EndDate { get => endDate; set => endDate = value; }
+
+        public double CalculateDailySpend()
+        {
+            DateTime startTime = startDate.ToDateTime(TimeOnly.MinValue);
+            DateTime endTime = endDate.ToDateTime(TimeOnly.MinValue).AddDays(1);
+            TimeSpan time = endTime - startTime;
+            double totalDays = time.TotalDays;
+            return amount / totalDays;
+        }
+        public double CalculateWeeklySpend()
+        {
+            return CalculateDailySpend() * 7;
+        }
+    }
+}
